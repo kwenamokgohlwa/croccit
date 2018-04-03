b499cf1350154e308f90d8c1b4809d3aebec4e3f
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-04-03 22:03:31 +0200

    Checkpoint Introduction to Express Complete

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const app = require("./app.js");
+const http = require("http");
+const server = http.createServer(app);
+
+server.listen(3000);
+
+server.on("listening", () => {
+  console.log("server is listening for requests on port 3000")
+});
