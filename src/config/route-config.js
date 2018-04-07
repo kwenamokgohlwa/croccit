e8fd83f1435712c233aee0922e86494342ff9e79
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-04-07 22:51:20 +0200

    Checkpoint Post Resource 2 Complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,10 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static.js");
     const topicRoutes = require("../routes/topics.js");
+    const postRoutes = require("../routes/posts.js");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
+    app.use(postRoutes);
   }
 }
