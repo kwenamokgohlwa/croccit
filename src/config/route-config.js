2eb6a7218780f56baa72c73ceea6f169d804b8d0
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-04-08 22:25:52 +0200

    Checkpoint Authentication Complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
     const staticRoutes = require("../routes/static.js");
     const topicRoutes = require("../routes/topics.js");
     const postRoutes = require("../routes/posts.js");
+    const userRoutes = require("../routes/users.js");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(postRoutes);
+    app.use(userRoutes);
   }
 }
